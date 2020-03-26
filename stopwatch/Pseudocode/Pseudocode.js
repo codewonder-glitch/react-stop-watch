2c86ef6ef1a9a8500989c297cc8a5e96e89ab507
Visa <visalakshi.cse@gmail.com>
2020-03-26 15:21:54 -0500

    Pseudocode file added

---
+++ stopwatch/Pseudocode/Pseudocode.js
@@ -0,0 +1,6 @@
+Three Buttons are created,Start,Stop and Pause
+With each button click specific functions are called
+Two state variables,timer,start are created
+With start for every one second timer is incremented by 1 using setinterval method
+With Stop start is set to flse and timer is set to 0
+With Pause if it is running paused using clearInterval method or called start method otherwise
